6a07d0052c08112388c08109e5c7cf2ea660215e
Sietse van Zanen <uglymotha@bookworm>
2026-08-15 11:21:04 +0200

    Systemd Integration
    This commit enables systemd integration on Linux.
     - configure.ac and Makefile.am where updated to check for systemd
     - --with-systemd (default=detect) was added to explicitely
       enable/disable systemd.
     - When it is detected that the daemon is started by systemd, the
       daemonize option will be ignored (service Type=simple)
     - Added sd_notify() at start-up and shut-down to notify systemd of status
     - Make use of the systemd watchdog (timeout = 30 secs), ping every
       10secs by adding a timeout to select() in the main event loop.
     - Modified check_standalone() to detect systemd socket activation.
     - Modified pure-authd and pure-certd and pure-uploadscript to detect
       and handle systemd startup. These check an environment variable to
       see if they are enabled and should run.
     - socket paths are hardcoded to /tmp/ (PrivateTmp=yes) and pid file
       paths are hardcoded to /run/pure-ftpd/ if daemons are started by
       systemd.
    
    TODO: Make template and installation for unit file.
    
    Date:      Sat Aug 15 11:21:04 2026 +0200
    Committer: Sietse van Zanen <uglymotha@bookworm>
    
    Changes to be committed:
    	modified:   Makefile.am
    	modified:   README
    	modified:   configure.ac
    	new file:   pure-ftpd.service
    	new file:   pure-ftpd.socket
    	new file:   pure-ftpd@.service
    	modified:   src/ftpd.c
    	modified:   src/globals.h
    	modified:   src/pure-authd.c
    	modified:   src/pure-authd_p.h
    	modified:   src/pure-certd.c
    	modified:   src/pure-certd_p.h
    	modified:   src/pure-uploadscript.c
    	modified:   src/pure-uploadscript_p.h
    	modified:   src/upload-pipe.h

--- src/upload-pipe.h
+++ src/upload-pipe.h
@@ -4,6 +4,8 @@
 #ifndef UPLOAD_PIPE_FILE
 # ifdef NON_ROOT_FTP
 #  define UPLOAD_PIPE_FILE CONFDIR "/pure-ftpd.upload.pipe"
+# elif defined USE_SYSTEMD
+#  define UPLOAD_PIPE_FILE systemd_init ? "/tmp/upload.pipe" : STATEDIR "/run/pure-ftpd.upload.pipe"
 # else
 #  define UPLOAD_PIPE_FILE STATEDIR "/run/pure-ftpd.upload.pipe"
 # endif
@@ -12,6 +14,8 @@
 #ifndef UPLOAD_PIPE_LOCK
 # ifdef NON_ROOT_FTP
 #  define UPLOAD_PIPE_LOCK CONFDIR "/pure-ftpd.upload.lock"
+# elif defined USE_SYSTEMD
+#  define UPLOAD_PIPE_LOCK systemd_init ? "/tmp/upload.lock" : STATEDIR "/run/pure-ftpd.upload.lock"
 # else
 #  define UPLOAD_PIPE_LOCK STATEDIR "/run/pure-ftpd.upload.lock"
 # endif
